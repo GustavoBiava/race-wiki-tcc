cf9b61a3af8c72c53151c05eb2c60b78c140f4c4
GustavoBiava <gustavo.rbiava@hotmail.com>
2024-08-12 15:48:02 -0300

    :tada: Commit inicial: Organizando diretórios

---
+++ Backend/src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { resolve } from 'path';
+
+import homeRoutes from './routes/homeRoutes';
+
+class App {
+
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }))
+    this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, 'static')));
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+
+}
+
+export default new App().app;
---
+++ Backend/src/server.js
@@ -0,0 +1,12 @@
+import app from '../src/app';
+import { config } from 'dotenv';
+
+config();
+
+const port = process.env.SERVER_PORT;
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
+
+
